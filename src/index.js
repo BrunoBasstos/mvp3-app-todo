72b98126cc36d1e43ec2efe1a3e0e2f37123199c
Bruno Bastos <brunomauro@gmail.com>
2023-09-07 21:00:04 -0300

    ajustes axios e formatacao

--- src/index.js
+++ src/index.js
@@ -2,19 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {DevSupport} from "@react-buddy/ide-toolbox";
-import {ComponentPreviews, useInitial} from "./dev";
-import axiosConfig from './axiosConfig';
-
-axiosConfig();
+import { DevSupport } from "@react-buddy/ide-toolbox";
+import { ComponentPreviews, useInitial } from "./dev";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <DevSupport ComponentPreviews={ComponentPreviews}
-                    useInitialHook={useInitial}
+            useInitialHook={useInitial}
         >
-            <App/>
+            <App />
         </DevSupport>
     </React.StrictMode>
 );
