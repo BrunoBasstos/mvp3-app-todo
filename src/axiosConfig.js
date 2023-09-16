2d4424efd9fa156d39e0406c5d369f358a84c2db
Bruno Bastos <brunomauro@gmail.com>
2023-09-16 18:33:42 -0300

    ajusta axios config

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 const TODO_API_URL = process.env.REACT_APP_TODO_API_URL || 'http://localhost:5000';
 const BRIDGE_API_URL = process.env.REACT_APP_BRIDGE_API_URL || 'http://localhost:5001';
 
-const token = localStorage.getItem('token');
 const commonHeaders = {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*',
@@ -13,7 +12,6 @@ const commonHeaders = {
 const todoApi = axios.create({
     baseURL: TODO_API_URL,
     headers: {
-        Authorization: `Bearer ${token}`,
         ...commonHeaders
     }
 });
