539bc815f350e7474fcd7ce70cf8e6a699b80483
Bruno Bastos <brunomauro@gmail.com>
2023-09-16 09:10:51 -0300

    remove autocomplete from email on login

--- src/components/Login.js
+++ src/components/Login.js
@@ -65,6 +65,7 @@ const Login = ({onLogin, toggleForm}) => {
                     type="email"
                     value={formData.email}
                     onChange={handleChange}
+                    autoComplete="off"
                 />
                 <TextField
                     fullWidth
